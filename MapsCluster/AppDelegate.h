045e9672bb8b6173ddddc7c65d634cb03d28f2af
Adam Barta <adam@flatcircle.io>
2016-06-14 11:13:54 +0200

    Initial Commit

---
+++ MapsCluster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapsCluster
+//
+//  Created by Adam Barta on 6/14/16.
+//  Copyright © 2016 PVT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapsCluster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapsCluster
+//
+//  Created by Adam Barta on 6/14/16.
+//  Copyright © 2016 PVT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
